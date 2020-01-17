563a5dcf7defdf905a57bd2686e1cc04a96ec7a0
slorber <lorber.sebastien@gmail.com>
2020-01-17 19:34:38 +0100

    eject

---
+++ bug-expokit-crashlytics/android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -0,0 +1,8 @@
+package host.exp.exponent.generated;
+
+// This file is auto-generated. Please don't rename!
+public class DetachBuildConstants {
+
+  public static final String DEVELOPMENT_URL = "exp185ea17e44434ab6b816e10a75c29856://192.168.0.13:19000";
+
+}
